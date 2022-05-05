807945d1a94ccf429a3ff98155129b98b83d65c6
mariana-oliveiraaa <99214209+mariana-oliveiraaa@users.noreply.github.com>
2022-05-05 11:34:11 -0300

    Add files via upload

---
+++ ProjetoIntegrador-vue/src/components/PrincipalComp.vue
@@ -0,0 +1,15 @@
+<script>
+import BlocoNovo from "@/components/BlocoNovo.vue"
+export default {
+    components: {BlocoNovo},
+};
+</script>
+<template>
+    <main id="content">
+        <BlocoNovo id="a" class="BlocoNovo" />
+        <BlocoNovo id="b" class="BlocoNovo"/>
+        <BlocoNovo id="c" class="BlocoNovo"/>
+        
+        
+    </main>
+</template>
